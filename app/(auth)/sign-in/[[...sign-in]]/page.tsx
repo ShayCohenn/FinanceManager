6d244c707745aab0a374820701b75bdf266e78d1
ShayCohenn <shay91847@gmail.com>
2024-06-01 20:36:41 +0300

    auth

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
-const page = () => {
+export default function Page() {
   return (
-    <div>page</div>
-  )
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-muted-foreground">
+            Welcome Back!
+          </h1>
+          <p className="text-base text-muted-foreground/80">
+            Login or Signup to get back to your dashboard
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className=""></div>
+    </div>
+  );
 }
-
-export default page
